77bc6764a867c4938f053236bc33377324c24524
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-06-08 14:48:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function sayHello(){
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!")
+		},1000)
+	})
+}
+let div=document.getElementById("output");
+let prom=sayHello();
+prom.then((data)=>{
+	div.innerText=data;
+})
